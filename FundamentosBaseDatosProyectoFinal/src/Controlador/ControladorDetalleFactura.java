8c4b089120082dbbb456b0618e49eb8bac47d43e
JustinLucero07 <justinluceroxd04@gmail.com>
2024-07-21 14:58:57 -0500

    Mi parte(Justin)

---
+++ FundamentosBaseDatosProyectoFinal/src/Controlador/ControladorDetalleFactura.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import DAO.DAODetalleFactura;
+import Modelo.Factura.DetalleFactura;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ControladorDetalleFactura {
+    
+    private DAODetalleFactura dAODetalleFactura;
+
+    public ControladorDetalleFactura() {
+        this.dAODetalleFactura = new DAODetalleFactura();
+    }
+    
+    public boolean ingresardetalle(DetalleFactura detalleFactura)throws SQLException{
+        return dAODetalleFactura.insertarDetalleFactura(detalleFactura);
+    }
+}
